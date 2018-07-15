879ce059ed33e405ad94b270a420d0fedeed09e9
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2018-07-14 23:48:17 -0400

    Add build status as param

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -48,11 +48,9 @@ def getAllFailedTests() {
 /**
  * Send Slack notification to the channel given based on buildStatus string
  */
-def call(String buildStatus, String channel = '#general', String branch = "", String commitMessage = "", String author = "") {
+def call(String buildStatus = "STARTED", String channel = '#general', String branch = "", String commitMessage = "", String author = "") {
 
   // build status of null means ongoing build
-  buildStatus = "$buildStatus" ?: 'STARTED'
-  echo currentBuild.result
   def attachments = []
   if (buildStatus == 'STARTED') {
     attachments = buildStartingMessage(branch, commitMessage, author)

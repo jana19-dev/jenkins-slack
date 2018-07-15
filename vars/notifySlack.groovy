e74a94bd0440cbd77c5c3914fa18256b72f0118f
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2018-07-14 23:27:58 -0400

    Fix current build status

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -51,7 +51,7 @@ def getAllFailedTests() {
 def call(String channel = '#general', String branch = "", String commitMessage = "", String author = "") {
 
   // build status of null means ongoing build
-  def buildStatus =  "$currentBuild .result" ?: 'STARTED'
+  def buildStatus =  "$currentBuild.result" ?: 'STARTED'
   def attachments = []
   if (buildStatus == 'STARTED') {
     attachments = buildStartingMessage(branch, commitMessage, author)

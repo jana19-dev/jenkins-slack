acdb958ef8e7e6fc05e06e8205594937423f8b7b
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2018-07-17 21:19:49 -0400

    Add single param as Map

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -5,99 +5,64 @@ import hudson.tasks.test.AbstractTestResultAction
 
 
 /**
- * Send Slack notification to SLACK_CHANNEL based on errorOccured
+ * Send a Slack notification based on given config values
  */
-def call(errorOccured = null) {
-  // errorOccured of null means ongoing build
-  def attachments = []
-  if (errorOccured == null) {
-    attachments = buildStartingMessage()
-  } else if (errorOccured == false) {
-    attachments = buildSuccessMessage()
-  } else {
-    attachments = buildFailureMessage(errorOccured)
+def call(Map config) {
+  status = config.get('status', 'STARTED')
+  message = config.get('message', '')
+  channel = config.get('channel', '#builds')
+  commitMessage = config.get('commitMessage', '')
+  commitAuthor = config.get('commitAuthor', '')
+  def color, text
+  def fields = [
+    [
+      title: "Commit by $commitAuthor",
+      value: commitMessage,
+      short: false
+    ]
+  ]
+
+  if (status == 'STARTED') {
+    color = "warning"
+    text = "Build Started :see_no_evil: :hear_no_evil: :speak_no_evil:"
+  } else if (status == 'SUCCESS') {
+    color = "good"
+    text = "Success after ${currentBuild.durationString} :awesome_dance: :disco_dance: :penguin_dance:"
+    fields.add([title: "Test Results", value: testSummary, short: true])
+  } else { // status == "FAILURE"
+    color = "danger"
+    text = "Failed after ${currentBuild.durationString} :crying_bear: :sad_pepe: :try_not_to_cry:"
+    fields.add([title: "Test Results", value: testSummary, short: true])
   }
-  notifySlack("", SLACK_CHANNEL, attachments)
-}
 
-
-def buildStartingMessage() {
-  return [
+  def summary = [
     [
       title: "$env.JOB_NAME-$env.BUILD_NUMBER",
       title_link: "$env.BUILD_URL",
-      color: "warning",
-      text: "Build Started :see_no_evil: :hear_no_evil: :speak_no_evil:",
-      fields: [
-        [
-          title: "Commit by $COMMIT_AUTHOR",
-          value: COMMIT_MESSAGE,
-          short: false
-        ]
-      ]
-    ]
-  ]
-}
-
-def buildSuccessMessage() {
-  def testSummary = getTestSummary()
-  return [
-    [
-      title: "$env.JOB_NAME-$env.BUILD_NUMBER",
-      title_link: "$env.BUILD_URL",
-      color: "good",
-      text: "Success after ${currentBuild.durationString} :awesome_dance: :disco_dance: :penguin_dance:",
-      fields: [
-        [
-          title: "Commit by $COMMIT_AUTHOR",
-          value: COMMIT_MESSAGE,
-          short: true
-        ],
-        [
-          title: "Test Results",
-          value: testSummary,
-          short: true
-        ]
-      ]
-    ]
-  ]
-}
-
-def buildFailureMessage(String globalError = "") {
-  def testSummary = getTestSummary()
-  return [
-    [
-      title: "$env.JOB_NAME-$env.BUILD_NUMBER",
-      title_link: "$env.BUILD_URL",
-      color: "danger",
-      text: "Failed after ${currentBuild.durationString} :crying_bear: :sad_pepe: :try_not_to_cry:",
+      color: color,
+      text: text,
       "mrkdwn_in": ["fields"],
-      fields: [
-        [
-          title: "Commit by $COMMIT_AUTHOR",
-          value: COMMIT_MESSAGE,
-          short: true
-        ],
-        [
-          title: "Test Results",
-          value: testSummary,
-          short: true
-        ]
-      ]
-    ],
-    [
-      title: "Error Details",
-      color: "danger",
-      text: '```'+"$globalError"+'```',
-      "mrkdwn_in": ["text"],
+      fields: fields
     ]
   ]
+  if (message != '') {
+    summary.add(
+      [
+        title: "More Details",
+        color: "warning",
+        text: '```'+"$message"+'```',
+        "mrkdwn_in": ["text"],
+      ]
+    )
+  }
+
+  sendMessage("", channel, summary)
 }
 
-def notifySlack(text, channel, attachments) {
+def sendMessage(text, channel, attachments) {
   def jenkinsIcon = 'https://wiki.jenkins-ci.org/download/attachments/2916393/logo.png'
   def payload = JsonOutput.toJson([
-    text: text,
+    text: "I WANT TO SE WHAT FOES IN HERE",
     channel: channel,
     username: "Jenkins",
     icon_url: jenkinsIcon,
